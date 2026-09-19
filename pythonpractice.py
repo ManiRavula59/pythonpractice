06efe7edc8abb9dc4bb930921d89c6f4347973fa
ManiRavula59 <tejakrishnamanikantaravula59@gmail.com>
2026-09-19 19:06:30 +0530

    session1

---
+++ pythonpractice.py
@@ -0,0 +1,8 @@
+name= input("Enter your name: ")
+age = int(input("Enter the age:  "))
+next_year_age= age+1
+
+print(f"hello {name} \nyou are {age} years old \nnext year you will be {next_year_age} ")
+a = int(input("Enter first number: " ))
+b = int(input("Enter second number: "))
+print(f"Sum: {a+b}\nDifference: {a-b}\nProduct: {a*b}\nDivison: {a/b}\nRemainder: {a%b}")
