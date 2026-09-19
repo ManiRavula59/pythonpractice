b6185b462906759c1f2c11cd7f263c8e36a49d6a
ManiRavula59 <tejakrishnamanikantaravula59@gmail.com>
2026-09-19 21:30:16 +0530

    session2

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,8 +1,26 @@
-name= input("Enter your name: ")
-age = int(input("Enter the age:  "))
-next_year_age= age+1
+# name= input("Enter your name: ")
+# age = int(input("Enter the age:  "))
+# next_year_age= age+1
 
-print(f"hello {name} \nyou are {age} years old \nnext year you will be {next_year_age} ")
-a = int(input("Enter first number: " ))
-b = int(input("Enter second number: "))
-print(f"Sum: {a+b}\nDifference: {a-b}\nProduct: {a*b}\nDivison: {a/b}\nRemainder: {a%b}")
+# print(f"hello {name} \nyou are {age} years old \nnext year you will be {next_year_age} ")
+# a = int(input("Enter first number: " ))
+# b = int(input("Enter second number: "))
+# print(f"Sum: {a+b}\nDifference: {a-b}\nProduct: {a*b}\nDivison: {a/b}\nRemainder: {a%b}")
+
+# #conditions
+
+# if age>= 18:
+#     print("Your are an Adult")
+# else:
+#     print("you are a minor")
+
+# number = int(input("Enter number:"))
+
+# if number%2==0:
+#     print(f"{number} is even")
+# else:
+#     print(f"{number}  is odd")
+number = int(input("Enter the number : "))
+
+for i in range(1,number + 1):
+    print(i)
